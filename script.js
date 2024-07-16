7709d50157d599fce028f52fae2d6bd158de7e23
Mohamed Nawfal <mohamednawfal183@gmail.com>
2024-07-16 16:57:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+1.
+// let cars=["rangerover","bmw","gwagon","defender"]
+// document.write(cars);
+2.
+// let cars=["rangerover","bmw","gwagon","defender"]
+// cars.unshift("bugatti");
+// cars.push("bmw")
+// document.write(cars)
+3.
+// let arr=[1,2,3,4,5]
+// document.write(arr.indexOf(5));
+// document.write(arr.includes(5));
+4.
+// let arr=[20,30,500,60,37]
+// document.write(arr.filter(i=>i%2==0));
+5.
+// let arr=[10,20,30,40]
+// document.write(arr.map(i=>i*i))
+6.
+// let arr=[2,6,4,9,3]
+// document.write(arr.sort())
+7.
+// let arr=[30,90,70,10]
+// document.write(arr.reverse())
+8.
+// let cars="defender"
+// document.write(cars.split(""))
